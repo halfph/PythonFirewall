baaf50f2e8023707ba005c0d3df9d289453145ed
Secqin <zhige311@hotmail.com>
2023-03-04 11:00:53 +0800

    add FireWall file for block ip for local

--- FireWall.py
+++ FireWall.py
@@ -1,9 +1,53 @@
+import os
+
+
 class Command:
+    CheckFireWallStatus = "netsh advfirewall show allprofiles"
+    TurnFireWallOn      = "netsh advfirewall set allprofiles state on"
+    TurnFireWallOff     = "netsh advfirewall set allprofiles state off"
+    ResetFireWall       = "netsh advfirewall reset"
+    BanIpByAll          = 'netsh advfirewall firewall add rule name="ban connectInternet1" ' \
+                          'dir=out remoteip=172.17.0.2 action=block'
+    BanIpByTcp          = 'netsh advfirewall firewall add rule name="ban connectInternet1" ' \
+                          'dir=out remoteip=172.17.0.2 action=block protocol=TCP'
+    BanIpByIcmpIvp4     = 'netsh advfirewall firewall add rule name="ban connectInternet1" ' \
+                          'dir=out remoteip=172.17.0.2 action=block protocol=icmpv4'
+    BanIpByIcmpIvp6     = 'netsh advfirewall firewall add rule name="ban connectInternet1" ' \
+                          'dir=out remoteip=172.17.0.2 action=block protocol=icmpv6'
+    BlackList = []
+
     def __int__(self):
         pass
 
     def ping(self, xxx: str):
         return str("ping " + xxx)
 
-command = Command()
-print(command.ping("119.91.141.182"))
+    def checkFireWallStatus(self):
+        os.system(self.CheckFireWallStatus)
+
+    # 需要管理员权限
+    def turnFireWallOn(self):
+        os.system(self.TurnFireWallOn)
+
+    def turnFireWallOff(self):
+        os.system(self.TurnFireWallOff)
+
+    def banIpByAll(self):
+        os.system(self.BanIpByAll)
+
+    def block(self):
+        self.turnFireWallOn()
+        self.banIpByAll()
+
+    def reset(self):
+        os.system(self.ResetFireWall)
+
+
+def BlockIP():
+    command = Command()
+    command.checkFireWallStatus()
+
+
+def Check():
+    command = Command()
+    command.checkFireWallStatus()
